d762f5576bad5d5ca64f8e482199af34e3aa3e8b
arisu2904 <arisusanto290401@gmail.com>
2025-08-09 12:03:06 +0700

    feat(medium-clone): init global filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './exceptions/httpException.filter';
+import { AllExceptionFilter } from './exceptions/allException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global exception filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new AllExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
